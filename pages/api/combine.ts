6bb748452ec3ac68fa3ee4e12c70a39a2fcbdcf2
Theo Sanderson <theo@theo.io>
2024-12-14 01:01:08 +0000

    upd

--- pages/api/combine.ts
+++ pages/api/combine.ts
@@ -33,15 +33,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-
+ 
   // Handle OPTIONS request
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
